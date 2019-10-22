c57947c905f75db8eadc56c92aa6f12d675de9cc
= <=>
2019-10-22 21:29:51 +0200

    Updates

---
+++ precache-manifest.192f68afcb378240bf32256b51949880.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2c064d4486e01598b2dd80e63c1edc9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f653d1ebc286e5cf0a76",
+    "url": "/static/css/main.460c98ca.chunk.css"
+  },
+  {
+    "revision": "0dbb1b7786173ee1bbb7",
+    "url": "/static/js/2.36ab1446.chunk.js"
+  },
+  {
+    "revision": "f653d1ebc286e5cf0a76",
+    "url": "/static/js/main.e7a519dc.chunk.js"
+  },
+  {
+    "revision": "0c97b3bef8d57f3a4532",
+    "url": "/static/js/runtime-main.23ad1654.js"
+  },
+  {
+    "revision": "df696f6829946af3abc093897fc49c1d",
+    "url": "/static/media/loader-azure.df696f68.svg"
+  },
+  {
+    "revision": "3de4de61f7b64c1d6dfb60cdf1dcc921",
+    "url": "/static/media/loader-button.3de4de61.svg"
+  },
+  {
+    "revision": "5f4bc2b1b3949f366ed31735e8a4bcf5",
+    "url": "/static/media/loader-earth.5f4bc2b1.svg"
+  },
+  {
+    "revision": "8835a88be51d17cf70e49711ace79f2a",
+    "url": "/static/media/loader-timer.8835a88b.svg"
+  }
+]);
