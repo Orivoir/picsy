f251889c578334d6a697dd40214bb78aa570d8fe
= <=>
2019-10-22 21:35:27 +0200

    Updates

---
+++ precache-manifest.2726470856583167bd2c7ab450520436.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18a83e623038f19a59c38fe76b36e353",
+    "url": "/picsy/index.html"
+  },
+  {
+    "revision": "801813124f8ec7bc790a",
+    "url": "/picsy/static/css/main.460c98ca.chunk.css"
+  },
+  {
+    "revision": "654d6f8c8603970e9bcd",
+    "url": "/picsy/static/js/2.a62a388b.chunk.js"
+  },
+  {
+    "revision": "801813124f8ec7bc790a",
+    "url": "/picsy/static/js/main.a404ca7b.chunk.js"
+  },
+  {
+    "revision": "5ae2d8438d885d38ffa4",
+    "url": "/picsy/static/js/runtime-main.7a6ef1b2.js"
+  },
+  {
+    "revision": "df696f6829946af3abc093897fc49c1d",
+    "url": "/picsy/static/media/loader-azure.df696f68.svg"
+  },
+  {
+    "revision": "3de4de61f7b64c1d6dfb60cdf1dcc921",
+    "url": "/picsy/static/media/loader-button.3de4de61.svg"
+  },
+  {
+    "revision": "5f4bc2b1b3949f366ed31735e8a4bcf5",
+    "url": "/picsy/static/media/loader-earth.5f4bc2b1.svg"
+  },
+  {
+    "revision": "8835a88be51d17cf70e49711ace79f2a",
+    "url": "/picsy/static/media/loader-timer.8835a88b.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.192f68afcb378240bf32256b51949880.js"
+  "/picsy/precache-manifest.2726470856583167bd2c7ab450520436.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/picsy/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
